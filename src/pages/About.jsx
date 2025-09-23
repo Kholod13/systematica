413ca506a79fdd87c0939b7527c885937419f9b7
Kholod13 <vlad13holod@gmail.com>
2025-09-23 23:03:24 +0200

    push project

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>It is page About</h1>
+      <p>This is the second page on your website.</p>
+    </div>
+  );
+}
+
+export default About;
